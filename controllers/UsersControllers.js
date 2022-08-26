47fa0b8dc93b375cfa37430a7ff71028dc002403
Camila <camilabentespessoa@gmail.com.br>
2022-08-26 14:53:14 -0300

    feat: add getOneUser

--- controllers/UsersControllers.js
+++ controllers/UsersControllers.js
@@ -9,6 +9,21 @@ class UsersController{
             return res.status(500).json(error.message)
         }
     }
+
+    static async getOneUser(req,res){
+        const { id } = req.params // recebe um parâmetro na requisição que é o id
+        try {
+            const oneUser = await database.Users.findOne({
+                where:{
+                    id: Number(id) //id na coluna do db
+                }
+            }) 
+            return res.status(200).json(oneUser)
+            
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
 }
 
 module.exports = UsersController
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,5 +4,6 @@ const UsersController = require('../controllers/UsersControllers')
 const router = Router()
 
 router.get('/users', UsersController.getAllUsers)
+router.get('/users/:id', UsersController.getOneUser)
 
 module.exports = router
